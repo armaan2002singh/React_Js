c1362a4319c11b2112a64ba2784e74f636900910
Armaan Singh <armaanaulakh2002@gmail.com>
2025-06-04 22:28:50 +0530

    Project1,2-basic(create-app & vite)

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>chai or react | Armaan singh</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+    <h1>chai or code react with vite | Armaan</h1>   
+  )
+}
+
+export default App
