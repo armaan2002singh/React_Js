01c79fe9d91b3bf24d6b64c39354939c61ec0253
Armaan Singh <armaanaulakh2002@gmail.com>
2025-06-07 21:05:57 +0530

    07react-router_50%

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-500'>ehllow</h1>
+    </>
+  )
+}
+
+export default App
