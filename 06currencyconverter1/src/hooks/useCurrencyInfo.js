c8fb96319ac99fe77a40f8450ac32df4efe28715
Armaan Singh <armaanaulakh2002@gmail.com>
2025-06-07 16:08:21 +0530

    Update_API in Project-6

--- 06currencyconverter1/src/hooks/useCurrencyInfo.js
+++ 06currencyconverter1/src/hooks/useCurrencyInfo.js
@@ -2,11 +2,12 @@
 // can use thr built in hooks.
 import {useEffect, useState} from "react"
 
-
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        const url = fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        const url = fetch(` //https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        // link to get free updates API-https://github.com/fawazahmed0/exchange-api?tab=readme-ov-file
+       
         .then((res) => res.json())
         .then((res) => setData(res[currency]))        
         console.log(data);
