655f00d7100fba32ffe33a89f0e18bac99116592
Armaan Singh <armaanaulakh2002@gmail.com>
2025-06-20 18:14:34 +0530

    Redux_config

--- 12megablog/src/App.jsx
+++ 12megablog/src/App.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+// now after having the service then on the loading of the application, useeffect used to check that app is logged in or not. 
+import {login, logout} from './store/authSlice'
+import {Header, Footer} from './components'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
-  console.log();
+  //state loading --> for conditional rendering.
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() =>{
+    //ask authservice which is current user.
+    authService.getCurrentUser()
+    .then((userData) =>{
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+        //console.log("user is log out");
+      }
+    })
+    .finally(() => setLoading(false))
+  })
+  // console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  // console.log();
   
   // it did not work because we made the application of vite but this implementation was of react.
   //so will use VITE_ and DB_PASSWORD = __
@@ -12,11 +36,15 @@ function App() {
   // then after having env file. we have settled-up of our appwrite platform 
   //then made a configuration file to make easy to access the varibales.
 
-  return (
-    <>
-      <h1>A blog app with appwrite.</h1>
-    </>
-  )
+  return !loading ? (<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+      todo will handle it when we will configure the react-router-dom {/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  </div>) : (null)
 }
 
 export default App
---
+++ 12megablog/src/store/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status :  false,
+    userData : null
+}
+const authSlice = createSlice({
+    name : "auth",
+    initialState,
+    reducers : {
+        login: (state, action) =>{ //action-1
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout : (state) =>{//action-2
+            state.status = false;
+            state.userData = null;
+        }
+    }
+})
+
+export const {login, logout} = authSlice.actions; // two actions are exported from the authSlice.
+export default authSlice.reducer;
