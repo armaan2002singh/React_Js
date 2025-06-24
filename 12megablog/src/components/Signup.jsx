21666e90cfdefeed79a15cbc453a22c87d83301f
Armaan Singh <armaanaulakh2002@gmail.com>
2025-06-25 00:58:10 +0530

    SignUp

--- 12megablog/src/components/Signup.jsx
+++ 12megablog/src/components/Signup.jsx
@@ -28,8 +28,50 @@ function Signup() {
         }
     }
   return (
-    <div>
-      
+    <div className='flex items-center justify-center'>
+      <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+            <h2 className='text-center text-2xl font-bold leading-tight'>
+              Sign in to your account
+            </h2>
+            <p className='mt-2 text-center text-base text-black/60'>
+            Already have an account?&nbsp;
+            <Link to='/signup'
+            className='font-medium text-primary transition-all duration-200 hover:underline'>
+              Sign In
+            </Link>
+            </p>
+            {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+            <form onSubmit={handleSubmit(create)}>
+                <div className='space-y-5'>
+                    <input 
+                    label="Full Name : "
+                    placeholder='Enter your full name'
+                    {...register("name",{
+                        required:true
+                    })}
+                    />
+                    <input type="email" 
+                    label="Email : "
+                    placeholder='Enter your email'
+                    {...register("email",{
+                        required:true,
+                        validate:{
+                            matchPatern: (value)=> /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/. 
+                            test(value) || "Email address must be a valid address",
+                        }
+                    })}
+                    />
+                    <input type="password"
+                    placeholder='Enter your password'
+                    label="Password"
+                    {...register("password",{
+                        required: true
+                    })} />
+                    <Button 
+                    type="submit" className="w-full">Create Account</Button>
+                </div>
+            </form>
+      </div>
     </div>
   )
 }
