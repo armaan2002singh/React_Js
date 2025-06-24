8b12263217b776cb4af77e68e6cab5e694197cd2
Armaan Singh <armaanaulakh2002@gmail.com>
2025-06-25 01:16:52 +0530

    AuthLayout

---
+++ 12megablog/src/components/AuthLayout.jsx
@@ -0,0 +1,42 @@
+// it is a mechanism to protect the pages.
+import React,{useEffect, useState} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+export default function Protected({
+    children,
+    authentication = true
+}) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(()=>{
+        //easy way 
+        /*
+        if(authStatus === true){
+        navigate("/")
+        }else if(authStatus === false){
+        navigate("/login")
+        }
+        */
+
+        //one more way -->
+        //  let authValue = authStatus === true ? true : false
+
+        //TODO make it more easy and understandable.
+        if(authentication && authStatus != authentication){
+            navigate("/login")
+        }else if(!authentication && authStatus != authentication){
+            navigate("/")
+        }
+
+        setLoader(false)
+    },[authStatus, navigate, authentication])
+
+  return loader? <h1>loading.....</h1> : <>{children}</>
+}
+
+
