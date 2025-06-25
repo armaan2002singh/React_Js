f5a34734855c89fab338c640e3e52d2ac1eed753
Armaan Singh <armaanaulakh2002@gmail.com>
2025-06-25 22:00:52 +0530

    Post-Form -- submit, slugTransform, And use of it

--- 12megablog/src/components/index.js
+++ 12megablog/src/components/index.js
@@ -1,11 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from './container/Container';
-import LogoutBtm from './Header/LogoutBtn'
+import LogoutBtm from './Header/LogoutBtn';
+import RTE from './RTE';
 
 export {
     Header,
     Footer,
     Container,
-    LogoutBtm
+    LogoutBtm,
+    RTE,
 }
